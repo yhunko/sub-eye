71f83ce5f273930cdc6d3a337daca2e6b92d8b55
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2025-11-25 16:26:41 +0200

    feat: adds basic dashboard UI

---
+++ src/features/dashboard/ui/dashboard-navbar.tsx
@@ -0,0 +1,16 @@
+import { UserDropdownMenu } from "../../auth/ui/user-dropdown-menu";
+import { DashboardLogo } from "./dashboard-logo";
+
+export const DashboardNavbar = () => {
+  return (
+    <header className="bg-background/60 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-40 w-full border-b backdrop-blur">
+      <div className="mx-auto flex h-14 max-w-screen-2xl items-center justify-between gap-3 px-4 md:gap-6 md:px-6">
+        <DashboardLogo />
+
+        <div className="ml-auto flex items-center gap-2 md:ml-0">
+          <UserDropdownMenu />
+        </div>
+      </div>
+    </header>
+  );
+};
