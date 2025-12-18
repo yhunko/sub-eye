90bf540d7a57822e921b704b5a591688c8b66a47
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2025-12-18 13:24:55 +0200

    feat: adds version number

--- src/features/dashboard/ui/dashboard-logo.tsx
+++ src/features/dashboard/ui/dashboard-logo.tsx
@@ -9,6 +9,9 @@ export const DashboardLogo = () => {
       <span className="text-sm font-semibold tracking-tight md:text-base">
         SubEye
       </span>
+      <span className="text-accent-foreground text-xs">
+        (v{process.env.NEXT_PUBLIC_APP_VERSION})
+      </span>
     </Link>
   );
 };
