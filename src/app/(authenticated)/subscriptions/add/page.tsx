242566c6a504d9f879426d48728fe1363eb2ed31
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2025-12-21 17:56:20 +0200

    feat(subscriptions): redesign form structure with modular components

--- src/app/(authenticated)/subscriptions/add/page.tsx
+++ src/app/(authenticated)/subscriptions/add/page.tsx
@@ -1,15 +1,12 @@
 import { DashboardNavbar } from "@/features/dashboard";
 import { DashboardLayout } from "@/features/dashboard/ui/dashboard.layout";
-import { Card, CardContent, CardHeader, CardTitle } from "@/shared/components";
+import { Card, CardContent } from "@/shared/components";
 import { AddSubscriptionForm } from "@/features/subscription";
 
 export default async function AddSubscriptionPage() {
   return (
     <DashboardLayout Navbar={<DashboardNavbar />}>
       <Card className="mx-auto max-w-screen-sm">
-        <CardHeader>
-          <CardTitle>Add subscription</CardTitle>
-        </CardHeader>
         <CardContent>
           <AddSubscriptionForm />
         </CardContent>
