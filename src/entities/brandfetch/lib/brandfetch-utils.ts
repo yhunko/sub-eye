78cebbca53df094d429473344b3ce09f98a699f1
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-01 16:56:28 +0200

    feat(brandfetch): centralize hostname constant, update CSP, and improve caching strategy

--- src/entities/brandfetch/lib/brandfetch-utils.ts
+++ src/entities/brandfetch/lib/brandfetch-utils.ts
@@ -1,7 +1,9 @@
 export class BrandfetchUtils {
+  static HOSTNAME = "cdn.brandfetch.io";
+
   static getImageUrl(domain: string, size: number = 80): string {
     const clientId = process.env.NEXT_PUBLIC_BRANDFETCH_CLIENT_ID;
 
-    return `https://cdn.brandfetch.io/${domain}/w/${size}/h/${size}/fallback/lettermark/type/icon?c=${clientId}`;
+    return `https://${this.HOSTNAME}/${domain}/w/${size}/h/${size}/fallback/lettermark/type/icon?c=${clientId}`;
   }
 }
