5c756fda856bd8b57ad6fe65b9b28f0075232ad7
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-05 16:59:44 +0200

    feat(settings): replace tabs with breadcrumbs and update layout components

--- src/app/[locale]/(protected)/(authenticated)/settings/notifications/page.tsx
+++ src/app/[locale]/(protected)/(authenticated)/settings/notifications/page.tsx
@@ -1,22 +1,32 @@
 import { DashboardLayout, DashboardNavbar } from "@/features/dashboard";
-import {
-  SettingsLayout,
-  SettingsTabs,
-  SettingsTab,
-  SettingsNotificationsForm,
-} from "@/features/settings";
+import { SettingsLayout, SettingsNotificationsForm } from "@/features/settings";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
   CardContent,
+  BreadcrumbSeparator,
+  BreadcrumbItem,
+  BreadcrumbPage,
 } from "@/shared/components";
+import { getTranslations } from "next-intl/server";
 
 export default async function SettingsNotificationsPage() {
+  const t = await getTranslations("settings");
+
   return (
     <DashboardLayout Navbar={<DashboardNavbar />}>
-      <SettingsLayout Tabs={<SettingsTabs tab={SettingsTab.NOTIFICATIONS} />}>
+      <SettingsLayout
+        Breadcrumbs={
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{t("pages.notifications")}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        }
+      >
         <Card>
           <CardHeader>
             <CardTitle>Notifications settings</CardTitle>
--- src/features/settings/ui/settings-layout.tsx
+++ src/features/settings/ui/settings-layout.tsx
@@ -1,20 +1,37 @@
 import { FC, PropsWithChildren, ReactNode } from "react";
 import { cn } from "@/shared/lib";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@/shared/components";
+import { useTranslations } from "next-intl";
 
 export type SettingsLayoutProps = {
-  Tabs: ReactNode;
+  Breadcrumbs: ReactNode;
   className?: string;
 };
 
 export const SettingsLayout: FC<PropsWithChildren<SettingsLayoutProps>> = ({
-  Tabs,
+  Breadcrumbs,
   className,
   children,
 }) => {
+  const t = useTranslations("settings");
+
   return (
     <div className="flex h-full w-full justify-center">
       <div className={cn("w-full max-w-xl space-y-4", className)}>
-        {Tabs}
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/settings">{t("title")}</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            {Breadcrumbs}
+          </BreadcrumbList>
+        </Breadcrumb>
 
         {children}
       </div>
