f6cb2b01880567bdec4a9e030be3b7d3f62002bf
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-06 12:35:46 +0200

    fix(pwa): normalized icons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,15 +21,8 @@ export const metadata: Metadata = {
   title: "SubEye",
   description: "Minimalist subscriptions tracker app",
   manifest: "/manifest.json",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "any" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
+  appleWebApp: {
+    title: "SubEye",
   },
 };
 
