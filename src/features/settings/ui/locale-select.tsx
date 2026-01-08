ac9daf9f7d2210b3e420a41c199e613ddab4d20a
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-08 12:07:26 +0200

    feat(i18n): implement translations for settings UI components

--- src/features/settings/ui/locale-select.tsx
+++ src/features/settings/ui/locale-select.tsx
@@ -7,15 +7,18 @@ import {
 } from "@/shared/components";
 import { Languages } from "lucide-react";
 import { LocaleSwitcher } from "../../i18n";
+import { useTranslations } from "next-intl";
 
 export const LocaleSelect = () => {
+  const t = useTranslations("settings.general.locale");
+
   return (
     <Item variant="outline">
       <ItemMedia>
         <Languages />
       </ItemMedia>
       <ItemContent>
-        <ItemTitle>Locale</ItemTitle>
+        <ItemTitle>{t("label")}</ItemTitle>
       </ItemContent>
       <ItemActions>
         <LocaleSwitcher />
