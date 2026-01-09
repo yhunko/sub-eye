1a88dbb9e0d51cafdd10a60e3aac3690191ffac1
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-09 18:19:41 +0200

    chore(db): remove legacy migrations; add Clerk URL

--- drizzle/0007_famous_prima.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "subscriptions" ALTER COLUMN "id" DROP DEFAULT;
-
-ALTER TABLE "subscriptions" ALTER COLUMN "id" SET DATA TYPE uuid USING gen_random_uuid();
-
-ALTER TABLE "subscriptions" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
