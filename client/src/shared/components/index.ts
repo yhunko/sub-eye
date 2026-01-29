714bb14fdda9ac3b07f4cc3dab06bde9f0a4a85a
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-01-29 23:04:06 +0200

    chore(subscriptions): add template `subscriptions/add` page route and UI components

--- client/src/shared/components/index.ts
+++ client/src/shared/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./ui/avatar";
 export * from "./ui/button";
+export * from "./ui/card";
 export * from "./ui/dropdown-menu";
 export * from "./ui/item";
 export * from "./ui/separator";
