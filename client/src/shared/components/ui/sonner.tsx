f365129eaec2899a46d19bbd63a4b8a6d95a8f6e
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 13:13:32 +0200

    fix: fixes sonner

--- client/src/shared/components/ui/sonner.tsx
+++ client/src/shared/components/ui/sonner.tsx
@@ -1,15 +1,15 @@
+import { useTheme } from "@/app/providers/theme-provider";
 import {
   CircleCheckIcon,
   InfoIcon,
   Loader2Icon,
   OctagonXIcon,
   TriangleAlertIcon,
-} from "lucide-react"
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, type ToasterProps } from "sonner"
+} from "lucide-react";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
@@ -32,7 +32,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
