6e6aa12bfa0c00ebd7e4d8516166029f31e145f4
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 19:51:29 +0200

    fix(pwa): attempt to fix pwa icon on iOS

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -47,23 +47,13 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "/assets/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/assets/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/assets/pwa-maskable-192x192.png",
+            src: "/assets/pwa/pwa-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "/assets/pwa-maskable-512x512.png",
+            src: "/assets/pwa/pwa-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
