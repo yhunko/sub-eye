f9ea6acf8fe3c805ec6f4ea4ad11d4db3053b861
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 14:22:14 +0200

    chore(ci): update deploy workflow with env variables

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   readonly APP_VERSION: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_CLERK_PUBLISHABLE_KEY: string;
+  readonly VITE_BRANDFETCH_CLIENT_ID: string;
 }
 
 interface ImportMeta {
