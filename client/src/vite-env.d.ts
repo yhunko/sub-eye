f47f21c6c10216f5390d28f61ab349c6ac3ab549
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 14:36:39 +0200

    feat(pwa): adjust PWA and icons config

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly APP_VERSION: string;
-  readonly VITE_API_URL: string;
+  readonly VITE_API_URL?: string;
   readonly VITE_CLERK_PUBLISHABLE_KEY: string;
   readonly VITE_BRANDFETCH_CLIENT_ID: string;
 }
