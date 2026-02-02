094cc4ce136d03d74cb4f9b01951fa94fa6e8965
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 11:20:42 +0200

    chore(ui): update splash screen layout; rename app title to SubEye

--- client/src/shared/ui/splash-screen.tsx
+++ client/src/shared/ui/splash-screen.tsx
@@ -2,10 +2,8 @@ import LogoWhite from "@/shared/assets/logo-white.svg";
 
 export function SplashScreen() {
   return (
-    <div className="flex h-svh items-center justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <img alt="logo" src={LogoWhite} className="size-32 animate-bounce" />
-      </div>
+    <div className="flex h-svh items-center justify-center bg-white!">
+      <img alt="logo" src={LogoWhite} className="size-32 animate-bounce" />
     </div>
   );
 }
