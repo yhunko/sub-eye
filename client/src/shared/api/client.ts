07da2596f5838dd690377a5a53c2bf4ef0241a8f
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2026-02-02 14:22:14 +0200

    chore(ci): update deploy workflow with env variables

--- client/src/shared/api/client.ts
+++ client/src/shared/api/client.ts
@@ -1,8 +1,6 @@
 import { honoClient } from "@server/client";
 
-const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "";
-
-export const apiClient = honoClient(SERVER_URL, {
+export const apiClient = honoClient(import.meta.env.VITE_API_URL, {
   fetch: (input: RequestInfo | URL, init?: RequestInit) =>
     fetch(input, {
       ...init,
